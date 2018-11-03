49fe2e83d2818d1692e25b48a48878d4860a8912
Tomasz Galecki <tomasz.galecki@hotmail.com>
2018-11-03 20:22:20 +0100

    Add docs for JobStateFetcher

--- Shapp/JobStateFetcher.cs
+++ Shapp/JobStateFetcher.cs
@@ -7,6 +7,15 @@ using Newtonsoft.Json;
 
 namespace Shapp
 {
+    /// <summary>
+    /// Class responsible for prepareing python script for fetching info about a job.
+    /// It uses base script from Properties.Resources.GetJobStatusScript.
+    /// Essencialy it puts job's clusterId and processNo into the string, which will be
+    /// interpreted as python script.
+    /// This task can be also done via just executing cli command like "condor_q" and "condor_history".
+    /// And no - IronPython can't be used. HTCondor python api is precomplied int .pyd, it can't
+    /// be used in IronPython environment.
+    /// </summary>
     class JobStateFetcher
     {
         private const string JOB_STATUS_PROPERTY_LABEL = "JobStatus";
@@ -16,6 +25,10 @@ namespace Shapp
         private readonly string PythonScriptWithFetcher;
         private readonly PythonScriptsExecutor pythonScriptExecutor;
 
+        /// <summary>
+        /// Constructs the status fetcher for one particular job id.
+        /// </summary>
+        /// <param name="jobId">job's id which state should be watched</param>
         public JobStateFetcher(JobId jobId)
         {
             JobId = jobId;
@@ -23,6 +36,10 @@ namespace Shapp
             pythonScriptExecutor = new PythonScriptsExecutor(PythonScriptWithFetcher);
         }
 
+        /// <summary>
+        /// Polls htcondor api for the status of the job defined in constructor.
+        /// </summary>
+        /// <returns>current job state</returns>
         public JobState GetJobState()
         {
             pythonScriptExecutor.Execute();
